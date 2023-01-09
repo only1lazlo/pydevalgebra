c982bd0737688541cb1bbd94475398956c121b6b
only1lazlo <114371222+only1lazlo@users.noreply.github.com>
2023-01-09 16:52:51 +0100

    Update racun_dict.py
    dodana funkcija za dodavanje stavki računa

--- racun_dict.py
+++ racun_dict.py
@@ -1,56 +1,68 @@
-def dodaj_racun(redni_broj):    
-    broj_racuna = f'BR-2023-01-{redni_broj}'
-    datum_izdavanja = '05.01.2023.'
-    stavke = [
-    {
-        "proizvod": "Mlijeko",
-        "cijena": 2.0,
-        "kolicina" : 5
-    },
-    {
-        "proizvod": "Kruh",
-        "cijena" : 2.0,
-        "kolicina" : 5
-    }
-    ]
-    iznos_pdv = 0
-    ukupan_iznos = 0
+class Racun:
+    """Ova klasa definira sve potrebno kako bi radili s računom"""
+    def __init__(self, broj_racuna, datum_izdavanja, stavke=None):
+        """
+        broj_racuna i datum_izdavanja su OBAVEZNI ARGUMENTI
+        stavke su OPCIONALNI ARGUMENT
+        ako se ne pošalju, bit će prazna lista
+        None je defaultna vrijednost
+        kao defaultne vrijednosti UVIJEK koristiti nepromijenjive vrijednosti
+        """
 
-    for stavka in stavke:
-        ukupan_iznos += stavka["cijena"]
+        self.broj_racuna = broj_racuna
+        self.datum_izdavanja = datum_izdavanja
+        #ako je korisnik unio stavke, uzmi tu vrijednost
+        if stavke:
+            self.stavke = stavke
+        else:
+            #inače nemamo stavke, odnosno
+            self.stavke = []
+        self.iznos_pdv = 0.0
+        self.ukupan_iznos = 0.0
+        
 
-    iznos_pdv = ukupan_iznos*0.25
-
-    racuni.append({
-        "broj_racuna": broj_racuna,
-        "datum_izdavanja": datum_izdavanja,
-        "stavke": stavke,
-        "iznos_pdv": iznos_pdv,
-        "ukupan_iznos" : ukupan_iznos
-
-    })
-
-racuni = []
-def ispisi_racune():
-    for racun in racuni:
+    def ispis_racuna(self):
+        print(f'Broj računa: {self.broj_racuna}')
+        print(f'Datum izdavanja: {self.datum_izdavanja}')
         print()
-        print(f'Broj računa: {racun["broj_racuna"]}')
-        print(f'Datum izdavanja: {racun["datum_izdavanja"]}')
-
-        print(f'Stavke računa:')
+        print("Stavke računa: ")
 
         print('\tProizvod\t\tCijena\t\tKolicina')
         print('-'*50)
-        for stavka in racun["stavke"]:
-            print(f'\t{stavka["proizvod"]}\t\t\t{stavka["cijena"]}\t\t{stavka["kolicina"]}')
-
-        print()
-        print(f'Ukupan iznos računa: {racun["ukupan_iznos"]}')
-        print(f'Iznos PDV: {racun["iznos_pdv"]}')
+        for stavka in self.stavke:
+            print(stavka)
         print()
 
+    def dodavanje_stavki(self, ime, br_cijena, br_kolicina):
+        self.stavke.append({"proizvod":ime, "cijena":br_cijena, "kolicina": br_kolicina})
 
-dodaj_racun(1)
-dodaj_racun(2)
-dodaj_racun(3)
-ispisi_racune()
+
+    #ZADATAK: napisati metodu u klasi Račun za dodavanje stavke
+    #pretpostavke: stavka se sastoji od imena proizvoda i cijene
+    #stavku spremiti u listu stavki (self.stavke) kao što smo to radili u prethodnom primjeru
+
+    #PRIMJER nakon dodavanja dvije stavke: 
+    """ stavke=[
+        {
+            "proizvod": "Mlijeko",
+            "cijena": 2.0,
+            "kolicina" : 5
+        },
+        {
+            "proizvod": "Kruh",
+            "cijena" : 2.0,
+            "kolicina" : 5
+        }
+    ]
+    """
+
+racun = Racun(1, '05.01.2023.', 
+        stavke=[{"proizvod": "Mlijeko",
+        "cijena": 2.0,
+        "kolicina" : 5
+        }])
+racun.ispis_racuna()
+
+racun_2 = Racun(2, '04.01.2023.')
+racun_2.dodavanje_stavki("Sir", 5.50, 1)
+racun_2.ispis_racuna()
